a2b1ff9b437d270a523024513a2e68e47dce9590
Bushetty <manojkumaracpt980@gmail.com>
2021-07-21 10:58:47 +0530

    adding day6 programs

---
+++ Backup/day6/add_function.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+function add()
+{
+	sum=$(($1+$2+$3))
+	return $sum
+}
+#calling function
+add 10 20 30
+$sum1=$1
+
+
---
+++ Backup/day6/powers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+Read -p Enter a number of the power:n
+x=2
+echo $x
+for((i=1;i<=n-1;i++))
+do
+x=$((x*2))
+echo$x
+done
+
+
